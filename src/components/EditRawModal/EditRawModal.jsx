fe1599609c99149abb45fdee31813825738fc946
Apprentice76 <hrk.som.01@gmail.com>
2022-04-25 17:01:11 +0530

    working?

--- src/components/EditRawModal/EditRawModal.jsx
+++ src/components/EditRawModal/EditRawModal.jsx
@@ -1,28 +1,52 @@
 import Cropper from 'react-perspective-cropper'
-import { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback, useEffect } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import axios from 'axios'
 
 const EditRawModal = ({ raw, showEditModal, handleEditClose, currentId }) => {
-	
+	const [img, setImg] = useState('')
+
+    useEffect(() => {
+        const bytes = new Uint8Array(raw?.data)
+        const file = new File([bytes], 'img.png', { type: 'image/png' })
+        console.log(file)
+        setImg(file)
+    }, [raw?.data])
+
+	// const base64 = ((buffer) => {
+	// 	// let bin = ''
+	// 	let bytes = new Uint8Array(buffer)
+	// 	// let len = bytes.length
+	// 	// for (let i = 0; i < len; i++) bin += String.fromCharCode(bytes[i])
+	// 	// localStorage.setItem('tempImgString', window.btoa(bin))
+	// 	// return bin
+    //     const file = new File([bytes], 'img.png', { type: 'image/*' })
+    //     console.log(file)
+	// 	return file
+	// 	// setImg(file)
+	// 	// return file
+	// 	// var binary = ''
+	// 	// var bytes = new Uint8Array(buffer)
+	// 	// var len = bytes.byteLength
+	// 	// for (var i = 0; i < len; i++) {
+	// 	// 	binary += String.fromCharCode(bytes[i])
+	// 	// }
+	// 	// return window.btoa(binary)
+	// })(raw?.data)
 	const [cropState, setCropState] = useState()
-    const [img, setImg] = useState('')
 	const [editDone, setEditDone] = useState(false)
 	const [uploaded, setUploaded] = useState(false)
-    const [blob, setBlob] = useState(null)
+	const [blob, setBlob] = useState(null)
 
     const cropperRef = useRef()
     
-    const base64 = (async (buffer) => {
-		// let bin = ''
-		let bytes = await new Uint8Array(buffer)
-		// let len = bytes.length
-		// for (let i = 0; i < len; i++) bin += String.fromCharCode(bytes[i])
-		// localStorage.setItem('tempImgString', window.btoa(bin))
-        return new File([bytes], 'img.jpg', { type: 'image/jpeg' })
-        // setImg(file)
-		// return file
-	})(raw?.data)
+    const editorCloser = () => {
+        setEditDone(false)
+        setUploaded(false)
+        setBlob(null)
+        setImg('')
+        handleEditClose(raw?.type)
+    }
 
 	const onDragStop = useCallback((s) => setCropState(s), [])
 	const onChange = useCallback((s) => setCropState(s), [])
@@ -40,41 +64,43 @@ const EditRawModal = ({ raw, showEditModal, handleEditClose, currentId }) => {
 	}
 
 	const sendEdited = () => {
-		const file = new File([blob], `${blob.name}`, { type: `${blob.type}` })
+        const file = new File([blob], `${blob.name}`, { type: `${blob.type}` })
+        
+        const formData = new FormData()
+        formData.append('raw', file)
+
 		axios
-			.post(
+			.put(
 				`${process.env.REACT_APP_BACKEND_URL}/uploadEdited/${currentId}/${raw.type}`,
+				formData,
 				{
-					raw: file,
-				},
-				{
-					headers: {
-						'Content-Type': 'multipart/form-data',
-					},
+					// headers: {
+					// 	'Content-Type': 'multipart/form-data',
+					// },
 				}
 			)
-            .then(() => {
-                setUploaded(true)
-                localStorage.removeItem('raw')
-            })
+			.then(() => {
+				setUploaded(true)
+				localStorage.removeItem('raw')
+			})
 			.catch((err) => {
 				console.log(err.message)
 			})
 	}
 
-	const onImgSelection = async (e) => {
-		if (e.target.files && e.target.files.length > 0) {
-			// it can also be a http or base64 string for example
-			setImg(e.target.files[0])
-	    } else if (e.target.file) {
-	        setImg(e.target.files)
-	    }
-	}
+	// const onImgSelection = async (e) => {
+	// 	if (e.target.files && e.target.files.length > 0) {
+	// 		// it can also be a http or base64 string for example
+	// 		setImg(e.target.files[0])
+	// 	} else if (e.target.file) {
+	// 		setImg(e.target.files)
+	// 	}
+	// }
 
 	return (
 		<Modal
 			show={showEditModal}
-			onHide={() => handleEditClose(raw?.type)}
+			onHide={() => editorCloser()}
 			backdrop='static'
 			keyboard={false}
 			aria-labelledby='contained-modal-title-vcenter'
@@ -90,17 +116,19 @@ const EditRawModal = ({ raw, showEditModal, handleEditClose, currentId }) => {
 				)} */}
 				{raw && (
 					<>
-						<Cropper
+                        {img !== '' && <Cropper
+                            maxHeight={500}
+                            maxWidth={300}
 							ref={cropperRef}
-							image={base64}
+							image={img}
 							onChange={onChange}
 							onDragStop={onDragStop}
-						/>
-						<input
+						/>}
+						{/* <input
 							type='file'
 							onChange={onImgSelection}
 							accept='image/*'
-						/>
+						/> */}
 						{!editDone && (
 							<Button onClick={doSomething}>Process Image</Button>
 						)}
@@ -113,7 +141,7 @@ const EditRawModal = ({ raw, showEditModal, handleEditClose, currentId }) => {
 			<Modal.Footer>
 				<Button
 					variant='primary'
-					onClick={() => handleEditClose(raw?.type)}
+					onClick={() => editorCloser()}
 					disabled={!uploaded}>
 					Done
 				</Button>
